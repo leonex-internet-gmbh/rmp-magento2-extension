c420daefe68b87077bfcc86c3d639815af9716da
Christian Stoller <christian.stoller@leonex.de>
2020-04-03 15:15:52 +0200

    Removed the payment selection for Magento 2.2 data update scripts, because it triggers the session start in CLI during upgrade process, which leads to errors

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -1,18 +1,8 @@
 <?php
-/**
- * This file is part of the Klarna Kp module
- *
- * (c) Klarna Bank AB (publ)
- *
- * For the full copyright and license information, please view the NOTICE
- * and LICENSE files that were distributed with this source code.
- */
 
 namespace Leonex\RiskManagementPlatform\Setup;
 
 use Leonex\RiskManagementPlatform\Setup\Patch\Data\PaymentMethodSelection;
-use Magento\Payment\Helper\Data as PaymentHelper;
-use Magento\Framework\App\State;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\UpgradeDataInterface;
@@ -22,21 +12,11 @@ use Magento\Framework\Setup\UpgradeDataInterface;
  */
 class UpgradeData extends PaymentMethodSelection implements UpgradeDataInterface
 {
-    public function __construct(PaymentHelper $paymentHelper, State $appState)
-    {
-        $this->paymentHelper = $paymentHelper;
-        $this->appState = $appState;
-    }
-
     /**
      * {@inheritDoc}
      */
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
         $this->moduleDataSetup = $setup;
-
-        if (version_compare($context->getVersion(), '2.0.1', '<')) {
-            $this->apply();
-        }
     }
 }
